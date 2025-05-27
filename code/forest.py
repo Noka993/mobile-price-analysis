61c17d80ac6a1af05ef0831771003af88c8cedb3
Michał Korzeniewski <michal.korzeniewskiii@gmail.com>
2025-05-27 12:20:25 +0200

    added basic forest model

---
+++ code/forest.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+from data import read_preprocessed_data
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_classification
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+from transform import transform_mobile_data
+X,y = read_preprocessed_data(std=True)
+
+
+
+
+train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3, random_state=42)
+
+clf = RandomForestClassifier(n_estimators=1000, max_depth=5,random_state=42)
+clf.fit(train_X, train_y)
+
+predict_y = clf.predict(test_X)
+
+print(clf.score(test_X, test_y))
+
+'''
+best_tree = None
+best_score = 0
+
+for i, tree in enumerate(clf.estimators_):
+    score = tree.score(test_X.to_numpy(), test_y.to_numpy())
+    if score > best_score:
+        best_score = score
+        best_tree = tree
+        best_index = i
+
+estimator = clf.estimators_[best_index] 
+'''
+
+estimator = clf.estimators_[0] 
+plt.figure(figsize=(20, 10))
+plot_tree(estimator, 
+          feature_names=clf.feature_names_in_, 
+          class_names=[str(cls) for cls in clf.classes_], 
+          filled=True, 
+          max_depth=4,
+          impurity=False)
+plt.show()
+
+importances = clf.feature_importances_
+features = clf.feature_names_in_
+
+forest_importances = pd.Series(importances, index=features)
+
+plt.figure(figsize=(10, 6))
+forest_importances.sort_values().plot(kind='barh')
+plt.title("Feature Importances")
+plt.tight_layout()
+plt.show()
